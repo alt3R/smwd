40f33287ec1d3f948c8a7ff1405e879d3a000423
alt3r <alt3r1985@gmail.com>
2021-06-16 15:32:48 +0300

    visitors and api client added

---
+++ app/controllers/concerns/api_client.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module ApiClient
+  extend ActiveSupport::Concern
+
+  SIGHTENGINE_API_URI = 'https://api.sightengine.com/1.0/check.json'
+  SIGHTENGINE_API_USER = Rails.application.credentials.dig(:keys, :sightengine, :user)
+  SIGHTENGINE_API_SECRET = Rails.application.credentials.dig(:keys, :sightengine, :secret)
+
+  SOCIAL_SEARCHER_API_URI = 'https://api.social-searcher.com/v2/search'
+  SOCIAL_SEARCHER_API_KEY = Rails.application.credentials.dig(:keys, :social_searcher, :key)
+
+  def send_request(addr, opts = nil, type: :json, method: 'get')
+    uri = URI(addr)
+    request = "Net::HTTP::#{method.capitalize}".constantize.new(uri)
+    request['Content-Type'] = 'application/json'
+    request.body = opts.to_json if opts.present?
+    response = Net::HTTP.start(uri.hostname) { |http| http.request(request) }
+    return standard(response.body) if type == :standard
+
+    json(response.body)
+  end
+
+  def list_of_users(username); end
+  
+  def find_weapon; end
+
+  def json(body)
+    JSON.parse(body).deep_symbolize_keys
+  end
+
+  def standard(body)
+    body
+  end
+end
---
+++ db/migrate/20210616123016_create_visitors.rb
@@ -0,0 +1,12 @@
+class CreateVisitors < ActiveRecord::Migration[6.1]
+  def change
+    create_table :visitors do |t|
+      t.datetime :online
+      t.jsonb :metadata, null: false, default: {}
+
+      t.timestamps
+    end
+
+    add_index :visitors, :metadata, using: :gin
+  end
+end
