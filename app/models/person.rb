b4ac537e8eecc6130a5cc4d16f5c69c921d2fa67
alt3r <alt3r1985@gmail.com>
2021-06-17 14:15:28 +0300

    fix error in show view

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,12 +1,4 @@
 class Person < ApplicationRecord
-  after_initialize :defaults, unless: :new_record?
-
   validates :full_name, presence: true, if: -> { username.blank? }
   validates :username, presence: true, if: -> { full_name.blank? }
-
-  def defaults
-    (attribute_names - %w[id created_at updated_at]).each do |attribute_name|
-      send("#{attribute_name}=", 'undefined') if send(attribute_name).blank?
-    end
-  end
 end
